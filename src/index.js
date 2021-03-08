0278a3fcfb6bbd285329716def22bb720a1ef964
Konstantin Saveljev <konstantin.saveljev@bolt.eu>
2021-03-08 23:08:39 +0200

    Capture action key presses (W, E, Q, S, D, A)

--- src/index.js
+++ src/index.js
@@ -51,6 +51,44 @@ const game = (p5) => {
         layout.origin = Point(p5.windowWidth / 2, p5.windowHeight / 2);
         p5.redraw();
     };
+
+    p5.keyPressed = () => {
+        switch (p5.keyCode) {
+            // W = north (top)
+            case 87:
+                console.log("W");
+                break;
+
+            // E = north-east (top-right)
+            case 69:
+                console.log("E");
+                break;
+
+            // Q = north-west (top-left)
+            case 81:
+                console.log("Q");
+                break;
+
+            // S = south (bottom)
+            case 83:
+                console.log("S");
+                break;
+
+            // D = south-east (bottom-right)
+            case 68:
+                console.log("D");
+                break;
+
+            // A = south-west (bottom-left)
+            case 65:
+                console.log("A");
+                break;
+
+            default:
+                console.log(`skipping ${p5.keyCode}`);
+                break;
+        }
+    };
 };
 
 new p5(game);
