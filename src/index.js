1c1fdb7db2652ae0f627ab40da0ce5524e6943f6
Konstantin Saveljev <konstantin.saveljev@bolt.eu>
2021-03-12 00:20:05 +0200

    Correctly set canvas size based on grid size

--- src/index.js
+++ src/index.js
@@ -23,18 +23,23 @@ import {
 } from "./util";
 
 const game = (p5) => {
-
-    const layout = Layout(
-        flatOrientation,
-        Point(60, 60),
-        Point(p5.windowWidth / 2, p5.windowHeight / 2)
-    );
-
     const radius = getRadiusFromHash() ?? 2;
     let grid = Grid(radius);
 
+    const config = {
+        size: 60,
+        width: Math.max(p5.windowWidth, (radius * 2 + 1) * 120),
+        height: Math.max(p5.windowHeight, (radius * 2 + 1) * 120)
+    };
+
+    const layout = Layout(
+        flatOrientation,
+        Point(config.size, config.size),
+        Point(config.width / 2, config.height / 2)
+    );
+
     p5.setup = async () => {
-        p5.createCanvas(p5.windowWidth, p5.windowHeight);
+        p5.createCanvas(config.width, config.height);
         //localSpawn(grid, 3);
         await remoteSpawn(grid);
     };
@@ -45,8 +50,10 @@ const game = (p5) => {
     };
 
     p5.windowResized = () => {
-        p5.resizeCanvas(p5.windowWidth, p5.windowHeight);
-        layout.origin = Point(p5.windowWidth / 2, p5.windowHeight / 2);
+        config.width = Math.max(p5.windowWidth, (radius * 2 + 1) * 120);
+        config.height = Math.max(p5.windowHeight, (radius * 2 + 1) * 120);
+        p5.resizeCanvas(config.width, config.height);
+        layout.origin = Point(config.width / 2, config.height / 2);
         p5.redraw();
     };
 
